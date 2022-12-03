bdcc51be97e264d36bbeefe5d7ec664b2c049d9a
felipsbreno <brendo.filipe2050@gmail.com>
2022-12-03 18:08:08 -0300

    WIP: add styles in app

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import { useMemo } from 'react';
 import { clusterApiUrl } from '@solana/web3.js';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+  PhantomWalletAdapter,
+  CoinbaseWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
 import {
   ConnectionProvider,
   WalletProvider,
@@ -14,7 +17,10 @@ import '@solana/wallet-adapter-react-ui/styles.css';
 function MyApp({ Component, pageProps }) {
   const network = WalletAdapterNetwork.Devnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new CoinbaseWalletAdapter()],
+    [],
+  );
 
   return (
     <ConnectionProvider endpoint={endpoint}>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>Solana NFTs</h1>;
+  const TWITTER_HANDLE = 'felipsbreno';
+  const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+  return (
+    <div className="h-screen overflow-hidden text-center bg-gray-900">
+      <div className="h-full flex flex-col justify-center p-6 relative text-white">
+        <div className="m-0 font-bold">
+          <p className="text-2xl">🍭 Candy Drop</p>
+          <p>Máquina de NFTs com cunhagem justa</p>
+        </div>
+        <div className="flex justify-center items-center absolute w-full bottom-4 left-0">
+          <Link
+            className="flex items-center"
+            href={TWITTER_LINK}
+            target="_blank"
+          >
+            <Image
+              src="/twitter-logo.svg"
+              alt="Image logo twitter"
+              width={35}
+              height={35}
+              quality={1000}
+            />
+            {`feito por @${TWITTER_HANDLE}`}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
